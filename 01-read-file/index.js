80688f34f617057343a5dc22c48f6e917b7b06cd
Alla-Petrova-Vitebsk <alla_petrova@mail.ru>
2021-10-29 09:22:11 +0300

    fix: task1 output without \n

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,9 +1,12 @@
 const path = require('path');
 const fs = require('fs');
+const {stdout} = process;
+
 
 const readStream = fs.createReadStream(  // create stream
    path.join(__dirname,'text.txt'), //path to file
    'utf-8'  
  );
- readStream.on('data',(chunk) => console.log(chunk));
+
+ readStream.on('data',(chunk) => stdout.write(chunk));
  readStream.on('error',(err) => console.log ('Error',err.message));
