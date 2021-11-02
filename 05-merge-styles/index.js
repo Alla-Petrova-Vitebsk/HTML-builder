fdff2d183954f533a8b94b89d285db13ffcb5bcf
Alla-Petrova-Vitebsk <alla_petrova@mail.ru>
2021-11-02 08:37:15 +0300

    fix: add comments

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,26 +4,25 @@ const path = require('path');
 const pathDirFrom = path.join(__dirname,'styles');
 const pathFileTo = path.join(__dirname,'project-dist','bundle.css');
 
-fs.writeFile(pathFileTo, //create 
+fs.writeFile(pathFileTo, //use writefile for create new empty file 'bundle.css' or rewrite if this file is exist
    '',
    (err) =>  {if (err) throw err;
    });
 
-
-fs.readdir(pathDirFrom,
-   {withFileTypes: true},
-   (err, items) => {
+fs.readdir(pathDirFrom,               //use readdir for read folder 'styles'
+   {withFileTypes: true},             //this option - for use check 'isFile'
+   (err, items) => {                  //callback for treatment array files and folders - 'items'  and  error  - 'err'
       if (err) throw err;
-      items.forEach(item => {
-         if (item.isFile()){
-            const pathFile = path.join(pathDirFrom,`${item.name}`);
-            const fileExt = path.extname(pathFile);
+      items.forEach(item => {         //  pass on files in folder
+         if (item.isFile()){          
+            const pathFile = path.join(pathDirFrom,`${item.name}`); //full path for file
+            const fileExt = path.extname(pathFile);   // receive extension file
             if(fileExt === '.css') {
-               fs.readFile(pathFile,
+               fs.readFile(pathFile,              //read file
                   'utf-8',
-                  (err,data) => {
+                  (err,data) => {       //callback for treatment content file - 'data' and error
                      if (err) throw err;
-                  fs.appendFile(pathFileTo,
+                  fs.appendFile(pathFileTo,  //use append for add 'data' into file 'bandle.css'
                      data,
                      (err) => {
                      if (err) throw err;
