01d57c74155dc79fb57146a3da838646a1232bd4
Alla-Petrova-Vitebsk <alla_petrova@mail.ru>
2021-11-04 16:55:28 +0300

    feat: update task06

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -4,6 +4,15 @@ const fsPromise = require('fs').promises;
 
 const pathTemplate = path.join (__dirname,'template.html');
 const pathDirTo = path.join (__dirname,'project-dist');
+const pathFileToHtml = path.join(__dirname, 'project-dist', 'index.html');
+
+//in this function use writefile for create new empty file or rewrite if this file is exist
+function createFile (path){ 
+   fs.writeFile(path, 
+   '',
+   (err) =>  {if (err) throw err;
+   });
+}
 
 
 //1. create folder 'project-dist'
@@ -12,46 +21,48 @@ fs.mkdir(pathDirTo,
    (err) => {if (err) throw err;
    });
 
-
-//2. 
-
+//2. create newHtml
+createFile(pathFileToHtml);
+//create html (string) from template and tags
 (async function (){
-
+ //content from Template into strTemplate (string)    
 const templateFile = async() => {
    const templateContent = await fsPromise.readFile(pathTemplate,'utf-8', (err) => {if (err) throw err});
    return await templateContent.toString(); 
 };
-let strTemplate = await templateFile(); //content template.html
-//console.log (str);
-let tags = strTemplate.match(/{{[a-z]+}}/gi);  //example: ['{{header}}',...]
- //console.log (tegs);
+let strTemplate = await templateFile(); //content from template.html
+//tag from strTemplate into array - tags
+const tags = strTemplate.match(/{{[a-z]+}}/gi);  //example: ['{{header}}',...]
+//circle on array with async function - replace template tags contents from files 
+ async function processArray (array) {
+  for (const tag of array) {
+      let tag1 =  tag.substring(2,tag.length - 2); //example: 'header'
+      let fileName = tag1 + '.html';
+      let pathTag = path.join(__dirname,'components', `${fileName}`);
+      /*  file from components*/
+      const tagFile = async() => {
+         const tagContent = await fsPromise.readFile(pathTag,'utf-8', (err) => {if (err) throw err});
+         return await tagContent.toString(); 
+      };
+      let strTag = await tagFile();
+      strTemplate = strTemplate.replace(tag,strTag);
+   }  
+     return await strTemplate;
+ };
+ const newHtml = await processArray(tags);
+ //console.log (newHtml);
 
-//д.б. цикл ****** 0 .. tags.length - 1
-let tag1 = tags[0].substring(2,tags[0].length - 2); //example: 'header'
-//console.log(teg1);
-let fileName = tag1 + '.html';
-let pathTag = path.join(__dirname,'components', `${fileName}`);
-//console.log (pathTag);
-
- /*  file from components*/
-const tagFile = async() => {
-   const tagContent = await fsPromise.readFile(pathTag,'utf-8', (err) => {if (err) throw err});
-   return await tagContent.toString(); 
-};
-let strTag = await tagFile();
-//console.log (strTag);
-
-let strNew = strTemplate.replace(tags[0],strTag);
-//console.log (strNew);
-
-
-
-
-//****** конец цикла 
-
+ //write into file content
+ fs.appendFile(pathFileToHtml,  //use append for add string 'newHtml' into file 'index.html'
+   newHtml,
+   (err) => {
+   if (err) throw err;
+   })
 })();
 
+
 //3.
+
 //4.
 
 
