cf5db0c7f3c2c89b5831c298357c2a516023ea6d
Alla-Petrova-Vitebsk <alla_petrova@mail.ru>
2021-11-05 15:14:24 +0300

    fix: task04 add del foles in folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,12 +4,27 @@ const path = require('path');
 const pathDirTo = path.join (__dirname,'files-copy');
 const pathDirFrom = path.join(__dirname,'files');
 
-//create folder 'files-copy'
+
+
+//1. create folder 'files-copy' 
 fs.mkdir(pathDirTo,
    {recursive:true},  //options where dir is exists
    (err) => {if (err) throw err;
    });
 
+//2. delete all files from folder  'files-copy' 
+fs.readdir(pathDirTo, (err, files) => {
+   if (err) throw err;
+    files.forEach(file => {
+     fs.unlink(path.join(pathDirTo, file), err => {
+       if (err) throw err;
+     });
+   });
+ });
+
+
+
+//3. copy files from 'files' to 'files-copy'
    fs.readdir(pathDirFrom,     //use readdir for read folder
    {withFileTypes: true},
    (err,items) => {
