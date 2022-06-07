271a40f4503aef027373321ceac7a76ae69e0ddf
Alla-Petrova-Vitebsk <alla_petrova@mail.ru>
2022-06-07 08:48:01 +0300

    feat: task01 var2

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,11 +2,18 @@ const path = require('path');
 const fs = require('fs');
 const {stdout} = process;
 
+//my
+// const readStream = fs.createReadStream(  // create stream
+//    path.join(__dirname,'text.txt'), //path to file
+//    'utf-8'  
+//  );
 
-const readStream = fs.createReadStream(  // create stream
-   path.join(__dirname,'text.txt'), //path to file
-   'utf-8'  
- );
+//  readStream.on('data',(chunk) => stdout.write(chunk));
+//  readStream.on('error',(err) => console.log ('Error',err.message));
 
- readStream.on('data',(chunk) => stdout.write(chunk));
- readStream.on('error',(err) => console.log ('Error',err.message));
+//stream
+const pathToFile = path.join(__dirname,'text.txt');
+const Stream = fs.createReadStream(pathToFile);
+Stream.pipe(stdout);
+
+
