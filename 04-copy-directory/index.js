4c9a8545846a54bffe590823b9b0330c36246ebc
Alla-Petrova-Vitebsk <alla_petrova@mail.ru>
2022-06-09 12:17:44 +0300

    feat: task4 var3 copy with subdir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,50 +1,50 @@
 const fs = require('fs/promises');
-const {join} = require('path');
+const { join } = require('path');
 
 const destinationDir = join(__dirname, 'files-copy');
 const sourceDir = join(__dirname, 'files');
 
 //create folder 'files-copy' 
-const createFolder = async () => {
+const createFolder = async (destinationDir) => {
    try {
       await fs.mkdir(destinationDir, { recursive: true });
-      console.log('create');
    } catch (err) {
       console.error(err);
    }
 }
 
 //delete folder  'files-copy' 
-const deleteFolder = async () => {
+const deleteFolder = async (destinationDir) => {
    try {
       await fs.rm(destinationDir, { recursive: true });
-      console.log('Delete');
    } catch (err) {
       console.error(err);
    }
 }
 
 //copy files from 'files' to 'files-copy'/
-const copyFiles = async () => {
+const copyFiles = async (sourceDir, destinationDir) => {
    try {
-      const itemsInDir = await fs.readdir(sourceDir,{withFileTypes:true});
+      await createFolder(destinationDir);
+      const itemsInDir = await fs.readdir(sourceDir, { withFileTypes: true });
       for (const item of itemsInDir) {
-         const sourceFile = join(sourceDir,`${item.name}`);
-         const destinationFile = join(destinationDir,`${item.name}`);
-         if (item.isFile())
-         await fs.copyFile(sourceFile,destinationFile);
+         const sourceFile = join(sourceDir, `${item.name}`);
+         const destinationFile = join(destinationDir, `${item.name}`);
+         if (item.isFile()) {
+            await fs.copyFile(sourceFile, destinationFile);
+         } else {
+            await copyFiles(sourceFile, destinationFile); //recursion
+         }
       }
-      console.log('Copied');
    } catch (err) {
       console.error(err);
    }
 }
 
 const copyDir = async () => {
-   await deleteFolder();
-   await createFolder();
-   await copyFiles();
-
+   await deleteFolder(destinationDir);
+   await copyFiles(sourceDir, destinationDir);
+   console.log('Copied');
 }
 
 copyDir();
